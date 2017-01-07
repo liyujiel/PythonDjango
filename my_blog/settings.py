9757f97d809ccc3d15d85a7d433597ad8c6cddf5
AlexLi <li1995717@gmail.com>
2017-01-06 16:17:12 -0800

    add buttons

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+#import markdown
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap_admin',
+#    'markup_deprecated',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
