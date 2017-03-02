c8b47aad5cc194390fbaf347ca6e0142341a4229
liyujiel <li1995717@gmail.com>
2017-03-02 14:11:52 -0800

    change wsgi

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -157,4 +157,5 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- my_blog/wsgi.py
+++ my_blog/wsgi.py
@@ -1,9 +1,12 @@
 """
-WSGI config for my_blog project.
+WSGI config for gettingstarted project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_blog.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
