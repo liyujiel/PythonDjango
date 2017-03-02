284c13feab02a3e6d855919101f5d7faf7ffe0a9
liyujiel <li1995717@gmail.com>
2017-03-02 12:04:28 -0800

    follow instruction change wsgi

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -133,18 +133,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- my_blog/wsgi.py
+++ my_blog/wsgi.py
@@ -7,13 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_blog.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
