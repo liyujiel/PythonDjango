6aadc1905dac6f4cd150aa34e5b20427db542dcb
liyujiel <li1995717@gmail.com>
2017-03-02 09:28:02 -0800

    Reinitialize

---
+++ README.md
@@ -0,0 +1,10 @@
+# PythonDjango
+Use Django build a blog
+
+
+http://localhost:9000/admin/
+
+
+http://localhost:9000/
+
+python3 manage.py runserver localhost:9000
---
+++ my_blog/urls.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import *
+from django.contrib import admin
+from article.models import Article
+from article.views import home,detail,archives,about_me,search_tag,blog_search,RSSFeed
+
+
+urlpatterns = (
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home, name ='home'),
+    url(r'^(?P<id>\d+)/$',detail, name ='detail'),
+    url(r'^archives/$',archives,name='archives'),
+    url(r'^aboutme/$',about_me,name='about_me'),
+    url(r'^tag(?P<tag>\w+)/$', search_tag, name = 'search_tag'),
+    url(r'^search/$', blog_search,name='search'),
+    url(r'^feed/$', RSSFeed(), name = "RSS"),  #新添加的urlconf, 并将name设置为RSS, 方便在模板中使用url
+#    url(r'^(?P<my_args>\d+)/$',detail, name ='detail'),
+#    url(r'^test/$', test, name = 'test'),
+)
